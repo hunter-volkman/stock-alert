3bdf4b8aa1ea22e78da0b61e17cf883523bf26bb
hunter-volkman <huntervolkman@protonmail.com>
2025-04-02 11:22:44 -0400

    Fix init

--- src/__init__.py
+++ src/__init__.py
@@ -6,9 +6,16 @@ from viam.components.sensor import Sensor
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 from .main import StockAlertEmail
 
-# Register the custom sensor model
+# Register the email sensor model
 Registry.register_resource_creator(
     Sensor.API,
     StockAlertEmail.MODEL,
     ResourceCreatorRegistration(StockAlertEmail.new, StockAlertEmail.validate_config)
+)
+
+# Register the SMS sensor model
+Registry.register_resource_creator(
+    Sensor.API,
+    StockAlertSMS.MODEL,
+    ResourceCreatorRegistration(StockAlertSMS.new, StockAlertSMS.validate_config)
 )
